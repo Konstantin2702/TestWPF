f7ed5ce05c1f58c5bb0290a10208c3f80f5be1da
Konstantin2702 <klaptev15337@gmail.com>
2023-12-05 12:21:09 +0300

    feat: Добавил модели для отображения в ListBox

---
+++ UI/Models/ControlInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace UI.Models
+{
+    public class ControlInfo
+    {
+        public ControlInfo(Guid guid, UserControl control) 
+        {
+            Guid = guid;
+            Control = control;
+        }
+
+        public Guid Guid { get; set; }
+
+        public UserControl Control { get; set; }
+    }
+}
---
+++ UI/Models/ErrorInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace UI.Models
+{
+    public class ErrorInfo
+    {
+
+        public Guid ModelGuid { get; set; }
+
+        public string PropertyName { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
