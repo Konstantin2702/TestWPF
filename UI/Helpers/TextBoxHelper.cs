f3e164c31681b3d66f2abb7bcc7fb6ac3c351423
Konstantin2702 <klaptev15337@gmail.com>
2023-12-05 12:21:49 +0300

    feat: Добавил обработчик валидации поля

---
+++ UI/Helpers/TextBoxHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using UI.ViewModels;
+
+namespace UI.Helpers
+{
+    public class TextBoxHelper
+    {
+        public static void HandleValidation(object sender, BaseViewModel viewModel, ValidationErrorEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+
+            var bindingName = textBox.GetBindingExpression(TextBox.TextProperty).ParentBinding.Path.Path;
+
+            string errorMessage;
+
+            if (e.Error.Exception != null)
+            {
+                errorMessage = "Заполните поле";
+            }
+            else
+            {
+                errorMessage = e.Error.ErrorContent.ToString();
+            }
+
+            viewModel.RootViewModel.HandleError(viewModel.Guid, bindingName, errorMessage, e.Action);
+        }
+    }
+}
